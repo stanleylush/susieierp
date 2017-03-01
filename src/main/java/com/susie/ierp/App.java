4f0623ae6135f200b8c4788ff31c0a31b03063a7
stanleylush <stanleylus@126.com>
2017-03-01 09:50:44 +0800

    创建项目
    Change-Id: If6603b43bb87960f59c90f490dd7d0ca0c9064c1
    Signed-off-by: stanleylush <stanleylus@126.com>

---
+++ src/main/java/com/susie/ierp/App.java
@@ -0,0 +1,13 @@
+package com.susie.ierp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
